69c6541c0cf317e8f53a1d2ecd26ff11d407a21d
sanketp2001 <sanket.p@angularminds.in>
2025-02-13 18:56:46 +0530

    updated

--- src/app/core/floral/floral.component.ts
+++ src/app/core/floral/floral.component.ts
@@ -1,50 +1,48 @@
+import { FormsModule } from '@angular/forms';
+import { WallpaperService } from './../../services/wallpaper/wallpaper.service';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { LoaderComponent } from '../../component/loader/loader.component';
 
 @Component({
   selector: 'app-floral',
-  imports: [],
+  imports: [FormsModule, CommonModule,LoaderComponent],
   templateUrl: './floral.component.html',
   styleUrl: './floral.component.css',
 })
 export class FloralComponent implements OnInit {
-  flowerImages: string[] = [];
+  wallpaperList: any;
+  isLoaded: boolean = false;
 
-  constructor() {}
-
-  ngOnInit(): void {
+  category: string = 'floral-pushpa';
+  constructor(private wallpaperService: WallpaperService,private router:Router) {
     this.generateFloralCollection();
   }
 
-  generateFloralCollection(): void {
-    // const container = document.getElementById('floralCollection');
+  ngOnInit() {
+    // this.generateFloralCollection();
+  }
 
-    // if (!container) {
-    //   console.error('Container element not found.');
-    //   return;
-    // }
+  generateFloralCollection() {
+    this.isLoaded = true;
 
-    // // Generate array of image names
-    // for (let i = 1; i <= 25; i++) {
-    //   this.flowerImages.push(`pushpa_${i}.jpg`);
-    // }
+    this.wallpaperService.getWallpaperByCategory(this.category).subscribe({
+      next: (data: any) => {
+        this.wallpaperList = data;
+        console.log(this.wallpaperList);
+        this.isLoaded = false;
 
-    // // Create and append image elements
-    // this.flowerImages.forEach((img, index) => {
-    //   const item = `
-    //     <div class="item features-image col-12 col-md-6 col-lg-3">
-    //       <div class="item-wrapper">
-    //         <div class="item-img mb-3">
-    //           <img src="assets/images/wallpapers/pushpa/${img}" alt="Try Refreshing this site..." data-slide-to="3" data-bs-slide-to="4">
-    //         </div>
-    //         <div class="item-content align-left">
-    //           <h5 class="item-title mbr-fonts-style mb-3 mt-0 display-7">
-    //             <!-- NZ-${137 + index} -->
-    //           </h5>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   `;
-    //   container.innerHTML += item;
-    // });
+      },
+      error: (error) => {
+        console.error('Error fetching wallpapers:', error);
+        this.isLoaded = false;
+
+      },
+    });
+  }
+
+  openWallpaper(wallpaperId:string){
+    this.router.navigate(['/wallpaper', wallpaperId]);
   }
 }
