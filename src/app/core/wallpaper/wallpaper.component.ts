2431cab99f44d7e1d23b3475ba4b1f674101bcaa
sanketp2001 <sanket.p@angularminds.in>
2025-02-14 19:15:35 +0530

    updates

--- src/app/core/wallpaper/wallpaper.component.ts
+++ src/app/core/wallpaper/wallpaper.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { WallpaperService } from './../../services/wallpaper/wallpaper.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-wallpaper',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './wallpaper.component.html',
-  styleUrl: './wallpaper.component.css'
+  styleUrl: './wallpaper.component.css',
 })
-export class WallpaperComponent {
+export class WallpaperComponent implements OnInit {
+  wallpaperData: any;
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private wallpaperService: WallpaperService
+  ) {}
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['wallpaperId']) {
+        console.log(params['wallpaperId']);
+        this.getWallpaper(params['wallpaperId']);
+      }
+    });
+  }
 
+  getWallpaper(wallpaperId: string) {
+    this.wallpaperService.getWallpaperById(wallpaperId).subscribe({
+      next: (data: any) => {
+        this.wallpaperData = data;
+        console.log(this.wallpaperData);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  back(){
+    history.back();
+  }
 }
