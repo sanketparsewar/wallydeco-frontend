e64c8d5eff55a57f044ddd7d4be78a1601d78f1c
Sanket Parsewar <sanket.parsewar97@gmail.com>
2025-02-16 05:04:03 +0530

    api updated

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiService {
-  private API_URL = 'http://localhost:8000/api';
+  // private API_URL = 'http://localhost:8000/api';
+  private API_URL = 'https://wallydeco-backend.onrender.com/api';
 
   constructor() {}
 
