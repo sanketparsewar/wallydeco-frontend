933321970c375b3466280e2ef783e74defdbb685
Sanket Parsewar <sanket.parsewar97@gmail.com>
2025-02-16 04:58:23 +0530

    updated the wallpaper details page

--- src/app/core/wallpaper/wallpaper.component.ts
+++ src/app/core/wallpaper/wallpaper.component.ts
@@ -1,21 +1,26 @@
 import { WallpaperService } from './../../services/wallpaper/wallpaper.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { LoaderComponent } from '../../component/loader/loader.component';
 
 @Component({
   selector: 'app-wallpaper',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, CommonModule,LoaderComponent],
   templateUrl: './wallpaper.component.html',
   styleUrl: './wallpaper.component.css',
 })
 export class WallpaperComponent implements OnInit {
   wallpaperData: any;
+  mainImage: string = '';
+  isLoaded: boolean = false;
+
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private wallpaperService: WallpaperService
-  ) {}
+  ) {
+  }
   ngOnInit() {
     this.activatedRoute.params.subscribe((params) => {
       if (params['wallpaperId']) {
@@ -25,19 +30,26 @@ export class WallpaperComponent implements OnInit {
     });
   }
 
+  onChangeMainImage(image: string) {
+    this.mainImage = image;
+  }
+
   getWallpaper(wallpaperId: string) {
+    this.isLoaded=true
     this.wallpaperService.getWallpaperById(wallpaperId).subscribe({
       next: (data: any) => {
         this.wallpaperData = data;
         console.log(this.wallpaperData);
+        this.isLoaded=false
       },
       error: (error) => {
         console.log(error);
+        this.isLoaded=false
       },
     });
   }
 
-  back(){
+  back() {
     history.back();
   }
 }
