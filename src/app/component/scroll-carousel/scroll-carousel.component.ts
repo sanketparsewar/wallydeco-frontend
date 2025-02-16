c32dc17855d5cdf15dbaef17841aa7acb592c12c
Sanket Parsewar <sanket.parsewar97@gmail.com>
2025-02-16 19:00:26 +0530

    update the image url

--- src/app/component/scroll-carousel/scroll-carousel.component.ts
+++ src/app/component/scroll-carousel/scroll-carousel.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WallpaperService } from '../../services/wallpaper/wallpaper.service';
+import { Router } from '@angular/router';
+import { AlertService } from '../../services/alert/alert.service';
 
 @Component({
   selector: 'app-scroll-carousel',
@@ -6,6 +9,35 @@ import { Component } from '@angular/core';
   templateUrl: './scroll-carousel.component.html',
   styleUrl: './scroll-carousel.component.css'
 })
-export class ScrollCarouselComponent {
-
+export class ScrollCarouselComponent implements OnInit {
+category:string='scrollable'
+ wallpaperList: any;
+   isLoaded: boolean = false;
+   constructor(
+     private wallpaperService: WallpaperService,
+     private router: Router,
+     private alertService: AlertService 
+   ) {}
+ 
+   ngOnInit() {
+     this.generateFloralCollection();
+   }
+ 
+   generateFloralCollection() {
+     this.isLoaded = true;
+     this.wallpaperService.getWallpaperByCategory(this.category).subscribe({
+       next: (data: any) => {
+         this.wallpaperList = data;
+         this.isLoaded = false;
+       },
+       error: (error) => {
+         this.alertService.showError(error.error.message);
+         this.isLoaded = false;
+       },
+     });
+   }
+ 
+   openWallpaper(wallpaperId: string) {
+     this.router.navigate(['/wallpaper', wallpaperId]);
+   }
 }
