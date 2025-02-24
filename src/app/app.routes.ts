ead283f3ad4ec9101f5ebfe2b1e4c2e30f52fd3b
sanketp2001 <sanket.p@angularminds.in>
2025-02-24 18:39:55 +0530

    new route of cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './core/auth/login/login.component';
 import { RegisterComponent } from './core/auth/register/register.component';
 import { AdminDashboardComponent } from './core/admin-dashboard/admin-dashboard.component';
 import { WallpaperComponent } from './core/wallpaper/wallpaper.component';
+import { CartComponent } from './core/cart/cart.component';
 // import { profileGuard } from './shared/guards/profile/profile.guard';
 // import { authGuard } from './shared/guards/auth/auth.guard';
 
@@ -39,12 +40,17 @@ export const routes: Routes = [
       {
         path:'admin-dashboard',
         component:AdminDashboardComponent
+      },
+      {
+        path:'cart',
+        component: CartComponent,
       }
     ],
   },
   {
     path: 'auth',
     // canActivate: [authGuard],
+    component: LayoutComponent,
     children: [
       {
         path: 'login',
