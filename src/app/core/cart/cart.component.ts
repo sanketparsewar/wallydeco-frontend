61ee0b7683cb4265b6c8c5d4c2f0880e55c5626d
sanketp2001 <sanket.p@angularminds.in>
2025-02-25 15:39:06 +0530

    updates

--- src/app/core/cart/cart.component.ts
+++ src/app/core/cart/cart.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
 
+
+
+
+  changeQuantity(qty:number){
+
+  }
 }
