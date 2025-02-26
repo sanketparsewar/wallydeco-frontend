272377c903f97ba1cf0ed40b72e2f04435fe927d
sanketp2001 <sanket.p@angularminds.in>
2025-02-26 16:31:44 +0530

    updted cart page

--- src/app/core/cart/cart.component.ts
+++ src/app/core/cart/cart.component.ts
@@ -1,39 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+// import { selectTotalPrice } from './../../shared/store/cart.selectors';
+import { Component, inject, OnInit } from '@angular/core';
 import { FooterComponent } from '../../component/footer/footer.component';
-import { CartService } from '../../services/cart/cart.service';
-import { Icart, IcartItem } from '../../shared/models/cart.interface';
+// import { Icart, IcartItem } from '../../shared/models/cart.interface';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { IcartItem } from '../../shared/store/cart.state';
+import {
+  selectCartItems,
+  selectCartTotal,
+} from '../../shared/store/cart.selectors';
+import {
+  clearCart,
+  removeItem,
+  updateItemQuantity,
+} from '../../shared/store/cart.actions';
+import { Router } from '@angular/router';
+// import {
+//   selectCartItems,
+//   selectTotalPrice,
+// } from '../../shared/store/cart.selectors';
+// import {
+//   decrementQuantity,
+//   incrementQuantity,
+//   removeFromCart,
+// } from '../../shared/store/cart.actions';
 
 @Component({
   selector: 'app-cart',
-  imports: [FooterComponent,CommonModule],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
-export class CartComponent implements OnInit{
+export class CartComponent implements OnInit {
   cartItems: IcartItem[] = [];
-  cart!:Icart;
-  constructor(private cartService: CartService) {}
+  orderObj: any = {
+    deliveryCharges: 200,
+  };
+  store = inject(Store);
+  router=inject(Router)
+  cartItems$: Observable<IcartItem[]> = this.store.select(selectCartItems);
+  cartTotal$: Observable<number> = this.store.select(selectCartTotal);
 
+  constructor() {}
   ngOnInit() {
-    this.getCart();
   }
-
-  getCart() {
-    this.cartService.cart$.subscribe({
-      next: (res) => {
-        this.cart = res;
-      },
-      error: (error) => {
-        console.log(error);
-      },
+  
+  removeItem(itemId: string) {
+    this.store.dispatch(removeItem({ itemId }));
+    localStorage.removeItem('cartItems');
+    this.cartItems$.subscribe((items: any) => {
+      localStorage.setItem('cartItems', JSON.stringify(items));
     });
   }
 
-  addQuantity(id:string) {
-    this.cartService.addQuantity(id);
+  updateQuantity(item: any, itemId: string, quantity: number) {
+    this.store.dispatch(updateItemQuantity({ itemId, quantity }));
   }
-  minusQuantity(id:string) {
-    this.cartService.minusQuantity(id);
+
+  clearCart() {
+    this.store.dispatch(clearCart());
+  }
+
+  openWallpaper(wallpaperId: string) {
+    this.router.navigate(['/wallpaper', wallpaperId]);
+  }
+  checkout() {
+    console.log(this.cartItems);
   }
 }
