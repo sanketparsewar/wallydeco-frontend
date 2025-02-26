189108b58bc44114b6423e050f1fd0f9d08927b6
sanketp2001 <sanket.p@angularminds.in>
2025-02-26 18:27:09 +0530

    added clear bag button

--- src/app/core/cart/cart.component.ts
+++ src/app/core/cart/cart.component.ts
@@ -1,3 +1,4 @@
+import { AlertService } from './../../services/alert/alert.service';
 // import { selectTotalPrice } from './../../shared/store/cart.selectors';
 import { Component, inject, OnInit } from '@angular/core';
 import { FooterComponent } from '../../component/footer/footer.component';
@@ -16,19 +17,11 @@ import {
   updateItemQuantity,
 } from '../../shared/store/cart.actions';
 import { Router } from '@angular/router';
-// import {
-//   selectCartItems,
-//   selectTotalPrice,
-// } from '../../shared/store/cart.selectors';
-// import {
-//   decrementQuantity,
-//   incrementQuantity,
-//   removeFromCart,
-// } from '../../shared/store/cart.actions';
-
+import { LoaderComponent } from '../../component/loader/loader.component';
+import { ConfirmService } from '../../services/confirm/confirm.service';
 @Component({
   selector: 'app-cart',
-  imports: [FooterComponent, CommonModule],
+  imports: [FooterComponent, CommonModule, LoaderComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
@@ -38,20 +31,27 @@ export class CartComponent implements OnInit {
     deliveryCharges: 200,
   };
   store = inject(Store);
-  router=inject(Router)
+  router = inject(Router);
   cartItems$: Observable<IcartItem[]> = this.store.select(selectCartItems);
   cartTotal$: Observable<number> = this.store.select(selectCartTotal);
-
-  constructor() {}
+  isLoaded: boolean = false;
+  constructor(
+    private confirmService: ConfirmService,
+    private alertService: AlertService
+  ) {}
   ngOnInit() {
+    this.isLoaded = true;
+    setTimeout(() => {
+      this.isLoaded = false;
+    }, 1000);
   }
-  
+
   removeItem(itemId: string) {
     this.store.dispatch(removeItem({ itemId }));
-    localStorage.removeItem('cartItems');
-    this.cartItems$.subscribe((items: any) => {
-      localStorage.setItem('cartItems', JSON.stringify(items));
-    });
+    // localStorage.removeItem('cartItems');
+    // this.cartItems$.subscribe((items: any) => {
+    //   localStorage.setItem('cartItems', JSON.stringify(items));
+    // });
   }
 
   updateQuantity(item: any, itemId: string, quantity: number) {
@@ -59,7 +59,12 @@ export class CartComponent implements OnInit {
   }
 
   clearCart() {
-    this.store.dispatch(clearCart());
+    this.confirmService.showConfirm('clear your bag').then((confirmed: any) => {
+      if (confirmed) {
+        this.store.dispatch(clearCart());
+        this.alertService.showSuccess('Bag Cleared.');
+      }
+    });
   }
 
   openWallpaper(wallpaperId: string) {
@@ -67,5 +72,9 @@ export class CartComponent implements OnInit {
   }
   checkout() {
     console.log(this.cartItems);
+  }
+
+  back() {
+    history.back();
   }
 }
