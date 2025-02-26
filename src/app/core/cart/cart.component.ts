e8b71cc71943897594e9f386144a2e14ed165f98
sanketp2001 <sanket.p@angularminds.in>
2025-02-26 12:59:22 +0530

    updated the cart page

--- src/app/core/cart/cart.component.ts
+++ src/app/core/cart/cart.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { CartService } from '../../services/cart/cart.service';
+import { Icart, IcartItem } from '../../shared/models/cart.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
-  imports: [FooterComponent],
+  imports: [FooterComponent,CommonModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+  cartItems: IcartItem[] = [];
+  cart!:Icart;
+  constructor(private cartService: CartService) {}
 
+  ngOnInit() {
+    this.getCart();
+  }
 
+  getCart() {
+    this.cartService.cart$.subscribe({
+      next: (res) => {
+        this.cart = res;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 
-
-  changeQuantity(qty:number){
-
+  addQuantity(id:string) {
+    this.cartService.addQuantity(id);
+  }
+  minusQuantity(id:string) {
+    this.cartService.minusQuantity(id);
   }
 }
