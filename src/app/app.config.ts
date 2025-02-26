8b40f49829778cbac475e7b4b6160023d5d1e84f
sanketp2001 <sanket.p@angularminds.in>
2025-02-26 16:32:00 +0530

    added provide state and store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,17 +5,23 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { cartReducer } from './shared/store/cart.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),  
+    provideHttpClient(),
     provideAnimations(),
     provideToastr({
-      timeOut: 2000,
-      positionClass: 'toast-bottom-right',
-      preventDuplicates: true,
-    })
-  ],
+        timeOut: 2000,
+        positionClass: 'toast-bottom-center',
+        preventDuplicates: true,
+    }),
+    provideStore(),
+    provideState({ name: 'cart', reducer: cartReducer }),
+    provideEffects()
+],
 };
