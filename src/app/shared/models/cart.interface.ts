7be2c2f453f6a2802e9e14f9977df650e328c5bd
sanketp2001 <sanket.p@angularminds.in>
2025-02-26 12:59:49 +0530

    cart service

---
+++ src/app/shared/models/cart.interface.ts
@@ -0,0 +1,18 @@
+export interface IcartItem {
+  id: string;
+  title: string;
+  image: string;
+  category: string;
+  stock: number;
+  size: string;
+  color?: string;
+  price: number;
+  quantity: number;
+  totalPrice: number;
+}
+
+export interface Icart {
+  items: IcartItem[];
+  totalPrice: number;
+  totalQuantity: number;
+}
