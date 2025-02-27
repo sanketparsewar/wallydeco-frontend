19021b5379994283f8f3b1bd488a2be53f2f903b
sanketp2001 <sanket.p@angularminds.in>
2025-02-27 18:11:48 +0530

    updated with the calling loggeduser api

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,31 +1,38 @@
-import { Component,OnInit} from '@angular/core';
+import { AuthService } from './../../services/auth/auth.service';
+import { Component, OnInit } from '@angular/core';
 import {
   ActivatedRoute,
   Router,
   RouterLink,
   RouterOutlet,
 } from '@angular/router';
-import { FooterComponent } from '../../component/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
-  imports: [
-    
-    RouterOutlet,
-    RouterLink,
-    CommonModule,
-    FormsModule,
-  ],
+  imports: [RouterOutlet, RouterLink, CommonModule, FormsModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
 export class LayoutComponent implements OnInit {
   currentRoute: String = '';
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
-  ngOnInit() {}
-
+  loggedUser: any;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+  ngOnInit() {
+    this.getLoggedUser()
+  }
+  getLoggedUser() {
+    this.authService.getLoggedUser().subscribe({
+      next: (data: any) => {
+        this.loggedUser = data.user;
+      },
+    });
+  }
   closeNavbar() {
     const navbar = document.getElementById('navbarSupportedContent');
     if (navbar?.classList.contains('show')) {
