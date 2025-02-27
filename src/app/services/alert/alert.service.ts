a0af6fc444eaa6319e8470f1bade6076d5b5cfcd
sanketp2001 <sanket.p@angularminds.in>
2025-02-27 18:10:39 +0530

    updated the sweetalert

--- src/app/services/alert/alert.service.ts
+++ src/app/services/alert/alert.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
@@ -7,17 +8,78 @@ import { ToastrService } from 'ngx-toastr';
 export class AlertService {
   constructor(private toastr: ToastrService) {}
 
-  showSuccess(message:string) {
-    this.toastr.success(message);
+  showSuccess(message: string) {
+    // this.toastr.success(message);
+   
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "success",
+      title: message
+    });
   }
 
-  showError(message:string) {
-    this.toastr.error(message);
+  showError(message: string) {
+    // this.toastr.error(message);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "error",
+      title: message
+    });
   }
-  showWarning(message:string) {
-    this.toastr.warning(message);
+  showWarning(message: string) {
+    // this.toastr.warning(message);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "warning",
+      title: message
+    });
   }
-  showInfo(message:string) {
-    this.toastr.info(message);
+  showInfo(message: string) {
+    // this.toastr.info(message);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "info",
+      title: message
+    });
   }
 }
