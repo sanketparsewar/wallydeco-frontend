5a8017ed12e7cf6f0957eef6b826dc67642dbab1
sanketp2001 <sanket.p@angularminds.in>
2025-04-14 11:59:32 +0530

    new component

---
+++ src/app/core/favourite/favourite.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { WallpaperService } from '../../services/wallpaper/wallpaper.service';
+import { Router } from '@angular/router';
+import { AlertService } from '../../services/alert/alert.service';
+import { LoaderComponent } from '../../component/loader/loader.component';
+import { FooterComponent } from '../../component/footer/footer.component';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-favourite',
+  imports: [LoaderComponent,FooterComponent,CurrencyPipe,FormsModule, CommonModule],
+  templateUrl: './favourite.component.html',
+  styleUrl: './favourite.component.css'
+})
+export class FavouriteComponent {
+wallpaperList: any;
+  isLoaded: boolean = false;
+
+  category: string = 'trending';
+  constructor(
+    private wallpaperService: WallpaperService,
+    private router: Router,
+    private alertService: AlertService
+  ) {
+    // this.generateTrendingCollection();
+  }
+
+  ngOnInit() {
+    // this.generateFloralCollection();
+  }
+  toggleFavorite(event: Event, item: any) {
+    const target = event.target as HTMLElement;
+    target.classList.toggle('text-danger'); // Toggle red color
+    target.classList.toggle('fa-solid'); // Change icon to solid heart
+    target.classList.toggle('fa-regular'); // Toggle back to regular heart
+  }
+  
+
+  generateTrendingCollection() {
+    this.isLoaded = true;
+
+    this.wallpaperService.getWallpaperByCategory(this.category).subscribe({
+      next: (data: any) => {
+        this.wallpaperList = data;
+        this.isLoaded = false;
+      },
+      error: (error) => {
+        this.alertService.showError(error.error.message);
+        this.isLoaded = false;
+      },
+    });
+  }
+
+  openWallpaper(wallpaperId: string) {
+    this.router.navigate(['/wallpaper', wallpaperId]);
+  }
+}
