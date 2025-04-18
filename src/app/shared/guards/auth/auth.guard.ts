50e5ef60e049ab763fb3de6fbe71cefcb3b3a1ef
sanketp2001 <sanket.p@angularminds.in>
2025-04-18 13:19:27 +0530

    updated guard

--- src/app/shared/guards/auth/auth.guard.ts
+++ src/app/shared/guards/auth/auth.guard.ts
@@ -1,18 +1,19 @@
+
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
 import { AlertService } from '../../../services/alert/alert.service';
 
 export const authGuard: CanActivateFn = () => {
-  const cookieService = inject(CookieService);
-  const alertService = inject(AlertService)
+  const alertService = inject(AlertService);
   const router = inject(Router);
-  const token = cookieService.get('accessToken');
-  if (!token) {
-    return true;
-  } else {
+
+  const token = localStorage.getItem('accessToken'); // No need for JSON.parse
+
+  if (token) {
+    alertService.showInfo('token')
     router.navigate(['/user/profile']);
     return false;
   }
 
+  return true;
 };
--- src/app/shared/guards/profile/profile.guard.ts
+++ src/app/shared/guards/profile/profile.guard.ts
@@ -1,18 +1,17 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-import { AlertService } from '../../../services/alert/alert.service';
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+import { AlertService } from "../../../services/alert/alert.service";
 
 export const profileGuard: CanActivateFn = () => {
-  const cookieService = inject(CookieService);
-  const alertService = inject(AlertService)
-
+  const alertService = inject(AlertService);
   const router = inject(Router);
-  const token = cookieService.get('accessToken');
-  if (token) {
-    return true;
-  } else {
+  const token = localStorage.getItem('accessToken'); // No need for JSON.parse
+
+  if (!token) {
+    alertService.showError('No access token found. Please login.');
     router.navigate(['/auth/login']);
     return false;
   }
+
+  return true;
 };
