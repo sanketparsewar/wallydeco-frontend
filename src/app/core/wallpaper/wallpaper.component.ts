d3849b545a188e6f74308c7826f2b23c7822dcd1
sanketp2001 <sanket.p@angularminds.in>
2025-04-22 16:46:48 +0530

    updated ui

--- src/app/core/wallpaper/wallpaper.component.ts
+++ src/app/core/wallpaper/wallpaper.component.ts
@@ -10,10 +10,12 @@ import { selectCartItems } from '../../shared/store/cart.selectors';
 import { IcartItem } from '../../shared/store/cart.state';
 import { Store } from '@ngrx/store';
 import { addItem } from '../../shared/store/cart.actions';
+import { FaqComponent } from '../../component/faq/faq.component';
+import { CategoryService } from '../../services/category/category.service';
 
 @Component({
   selector: 'app-wallpaper',
-  imports: [CurrencyPipe, CommonModule, LoaderComponent, FooterComponent],
+  imports: [CurrencyPipe, CommonModule, LoaderComponent, FooterComponent, FaqComponent],
   templateUrl: './wallpaper.component.html',
   styleUrl: './wallpaper.component.css',
 })
@@ -25,29 +27,68 @@ export class WallpaperComponent implements OnInit {
 
   store = inject(Store);
   cartItems$: Observable<IcartItem[]> = this.store.select(selectCartItems);
+  features: any[] = [
+    { img: 'https://img.icons8.com/color/48/rgb-circle-1--v1.png', text: 'Shiny Colors' },
+    { img: 'https://img.icons8.com/dotty/80/gloss-paper.png', text: 'Top Quality Material' },
+    { img: 'https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/external-man-male-profession-vitaliy-gorbachev-lineal-vitaly-gorbachev-44.png', text: 'Professional Installation' },
+    { img: 'https://img.icons8.com/ios/50/cleaning-a-surface.png', text: 'Easy to Clean' },
+    { img: 'https://img.icons8.com/dotty/80/consultation.png', text: 'Expert Consultation' },
+    { img: 'https://img.icons8.com/pastel-glyph/64/security-checked--v2.png', text: 'Damage Warranty' }
+  ]
+  wallpaperList: any[] = [];
+
+  categories = []
+  category: string = '';
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private wallpaperService: WallpaperService,
-    private alertService: AlertService
-  ) {}
+    private alertService: AlertService,
+    private categoryService: CategoryService,
+
+  ) {
+    this.categoryService.categories$.subscribe((data: any) => {
+      this.categories = data.map((cat: any) => cat.name);
+      this.category = this.categories[Math.floor(Math.random() * this.categories.length)];
+      this.generateCollection()
+    });
+  }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params) => {
-      if (params['wallpaperId']) {
-        this.getWallpaper(params['wallpaperId']);
+      if (params['id']) {
+        this.getWallpaper(params['id']);
       }
     });
   }
+
+
+  generateCollection() {
+    // Get trending wallpapers
+    this.wallpaperService.getWallpaperByCategory(this.category).subscribe({
+      next: (data: any) => {
+        this.wallpaperList = data;
+      },
+      error: (error) => {
+        this.alertService.showError(error.error.message);
+      },
+    });
+  }
+
+
+  openWallpaper(id: string) {
+    this.router.navigate(['/wallpaper', id]);
+  }
+
 
   onChangeMainImage(image: string) {
     this.mainImage = image;
   }
 
-  getWallpaper(wallpaperId: string) {
+  getWallpaper(id: string) {
     this.isLoaded = true;
-    this.wallpaperService.getWallpaperById(wallpaperId).subscribe({
+    this.wallpaperService.getWallpaperById(id).subscribe({
       next: (data: any) => {
         this.wallpaperData = data;
         this.isLoaded = false;
@@ -58,6 +99,7 @@ export class WallpaperComponent implements OnInit {
       },
     });
   }
+
   selectColor(event: any) {
     this.selectedColor = event.target.value;
   }
@@ -71,6 +113,7 @@ export class WallpaperComponent implements OnInit {
       category: this.wallpaperData.category,
       stock: this.wallpaperData.stock,
       size: this.wallpaperData.size,
+      wallpaperId:this.wallpaperData.wallpaperId,
       color: this.selectedColor,
       quantity: 1, // Default to quantity 1
     };
@@ -79,7 +122,7 @@ export class WallpaperComponent implements OnInit {
     this.cartItems$.subscribe(
       (items: any) => {
         localStorage.setItem('cartItems', JSON.stringify(items));
-      }     
+      }
     );
   }
   openCart() {
