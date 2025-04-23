5d9e2b8cf79c2d61cf8aeed70e47e07ac23784a0
sanketp2001 <sanket.p@angularminds.in>
2025-04-23 15:37:11 +0530

    fetched the admin dashboard data

--- src/app/core/admin-dashboard/admin-dashboard.component.ts
+++ src/app/core/admin-dashboard/admin-dashboard.component.ts
@@ -6,6 +6,9 @@ import { AddWallpaperComponent } from '../../shared/modals/add-wallpaper/add-wal
 import { WallpaperDataTableComponent } from '../../component/wallpaper-data-table/wallpaper-data-table.component';
 import { OrderDataTableComponent } from '../../component/order-data-table/order-data-table.component';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { OrderService } from '../../services/order/order.service';
+import { DashboardService } from '../../services/dashboard/dashboard.service';
+import { IDashboard } from '../../shared/models/dashboard.interface';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -20,15 +23,47 @@ import { FooterComponent } from '../../component/footer/footer.component';
   styleUrl: './admin-dashboard.component.css',
 })
 export class AdminDashboardComponent implements OnInit {
-  tableData: any;
   option: string = 'orders';
-  // option: string = 'wallpapers';
+  dashboardData: IDashboard = {
+    totalCancelledOrders: 0,
+    totalCities: 0,
+    totalCoupons: 0,
+    totalDeliveredOrders: 0,
+    totalOrders: 0,
+    totalPendingOrders: 0,
+    totalSales: 0,
+    totalShippedOrders: 0,
+    totalSold: 0,
+    totalStock: 0,
+    totalUsedCoupons: 0,
+    totalUsers: 0,
+    totalWallpapers: 0,
+    totalCategories: 0,
+  };
 
-  constructor(private wallpaperService: WallpaperService) {}
+  constructor(private dashboardService: DashboardService) { }
 
   onOption(option: string) {
     this.option = option;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getDashboardData()
+  }
+
+
+  getDashboardData() {
+    this.dashboardService.getDashboardData().subscribe({
+      next: (data: any) => {
+        this.dashboardData = data
+        console.log(this.dashboardData);
+        this.dashboardData.totalOrders = data.totalOrders;
+        this.dashboardData.totalSales = data.totalSales;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
 }
