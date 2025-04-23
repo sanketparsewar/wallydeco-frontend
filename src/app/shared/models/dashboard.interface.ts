c9b7fdcb1c885fac6ab0e3fb3a1650c6abc267b1
sanketp2001 <sanket.p@angularminds.in>
2025-04-23 15:33:54 +0530

    dashboardData interface model

---
+++ src/app/shared/models/dashboard.interface.ts
@@ -0,0 +1,16 @@
+export interface IDashboard {
+    totalCancelledOrders:number;
+    totalCities:number;
+    totalCoupons:number;
+    totalDeliveredOrders:number;
+    totalOrders:number;
+    totalPendingOrders:number;
+    totalSales:number;
+    totalShippedOrders:number;
+    totalSold:number;
+    totalStock:number;
+    totalUsedCoupons:number;
+    totalUsers:number;
+    totalWallpapers:number;
+    totalCategories:number;
+}
