54e58038de83a81e1506439d6b3b8a32a600a69f
sanketp2001 <sanket.p@angularminds.in>
2025-04-23 18:35:08 +0530

    updated tables and child components

--- src/app/core/admin-dashboard/admin-dashboard.component.ts
+++ src/app/core/admin-dashboard/admin-dashboard.component.ts
@@ -9,6 +9,10 @@ import { FooterComponent } from '../../component/footer/footer.component';
 import { OrderService } from '../../services/order/order.service';
 import { DashboardService } from '../../services/dashboard/dashboard.service';
 import { IDashboard } from '../../shared/models/dashboard.interface';
+import { LoaderComponent } from '../../component/loader/loader.component';
+import { CouponComponent } from '../../component/coupon/coupon.component';
+import { CityComponent } from '../../component/city/city.component';
+import { CategoryComponent } from '../../component/category/category.component';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -17,28 +21,35 @@ import { IDashboard } from '../../shared/models/dashboard.interface';
     FormsModule,
     WallpaperDataTableComponent,
     OrderDataTableComponent,
-    FooterComponent,
+    FooterComponent,LoaderComponent,
+    CouponComponent,
+    CityComponent,
+    CategoryComponent
   ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
 })
 export class AdminDashboardComponent implements OnInit {
   option: string = 'orders';
+  isLoaded: boolean = false;
+
   dashboardData: IDashboard = {
-    totalCancelledOrders: 0,
-    totalCities: 0,
-    totalCoupons: 0,
-    totalDeliveredOrders: 0,
     totalOrders: 0,
-    totalPendingOrders: 0,
     totalSales: 0,
+    totalPendingOrders: 0,
+    totalDeliveredOrders: 0,
     totalShippedOrders: 0,
-    totalSold: 0,
-    totalStock: 0,
-    totalUsedCoupons: 0,
-    totalUsers: 0,
+    totalCancelledOrders: 0,
     totalWallpapers: 0,
+    totalStock: 0,
+    totalSold: 0,
+    totalCoupons: 0,
+    totalActiveCoupons:0,
+    totalInactiveCoupons:0,
+    totalUsedCoupons: 0,
     totalCategories: 0,
+    totalCities: 0,
+    totalUsers: 0,
   };
 
   constructor(private dashboardService: DashboardService) { }
@@ -53,14 +64,18 @@ export class AdminDashboardComponent implements OnInit {
 
 
   getDashboardData() {
+    this.isLoaded = true;
     this.dashboardService.getDashboardData().subscribe({
       next: (data: any) => {
+        this.isLoaded = false;
         this.dashboardData = data
         console.log(this.dashboardData);
         this.dashboardData.totalOrders = data.totalOrders;
         this.dashboardData.totalSales = data.totalSales;
       },
       error: (error) => {
+        this.isLoaded = false;
+
         console.log(error);
       },
     });
