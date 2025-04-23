2e505693bb4a9730c25d898be51c2fd55f27414c
sanketp2001 <sanket.p@angularminds.in>
2025-04-23 15:37:50 +0530

    getting the city list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth/auth.service';
 import { UserService } from './services/user/user.service';
 import { CategoryService } from './services/category/category.service';
+import { CityService } from './services/city/city.service';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,12 @@ import { CategoryService } from './services/category/category.service';
 })
 export class AppComponent {
   title = 'angular-wallydeco';
-  constructor(private userService: UserService, private categoryService: CategoryService) { }
+  constructor(private cityService:CityService, private userService: UserService, private categoryService: CategoryService) { }
 
   ngOnInit() {
     this.getUser()
     this.getAllCategories()
+    this.getAllCities()
   }
 
 
@@ -31,6 +33,13 @@ export class AppComponent {
     this.categoryService.getAllCategories().subscribe({
       next: () => console.log('categories fetched'),
       error: () => console.log('error fetching categories'),
+    });
+  }
+
+  getAllCities(){
+    this.cityService.getAllCities().subscribe({
+      next: () => console.log('cities fetched'),
+      error: () => console.log('error fetching cities'),
     });
   }
 
